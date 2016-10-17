3af0d90fc033133fff2736a869d3bb9b6c380653
Angelo Pearce <angelopearce85@gmail.com>
2016-10-17 16:14:19 +0200

    func save

--- dates.js
+++ dates.js
@@ -14,26 +14,3 @@ var isWeekday = function (day){
   console.log(angelo);
 
 
-/*DATES:
-
-Using the Javascript Date object to write the functions below.
-
-This code contains some of the building logic you need for the functions.
-
-const date = new Date('2010-04-01');
-var weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Saturday'];
-
-//return index between 0 and 6
-console.log(date.getDay());
-
-//look up the week day text string from the Array
-console.log(weekDays[date.getDay()]);
-Write a function called isWeekday find out if the parameter passed in is a weekDay.
-
-Write a function called yearsAgo that takes in a year and return how many years ago that was. Use the Date objects getFullYear function.
-
-Write a function called dayToday to find the day today. This function takes no parameters;
-
-Write a function called getDay which can find out which day of the week a specific date was.
-
-Write a function called sameWeekday that takes in two dates and check if both dates have the same weekday. For example if both dates are a Monday the function should return true.*
---
+++ set-interval.js
@@ -0,0 +1,21 @@
+var counter = 0;
+var number;
+
+function showNumber(num){
+    if (!number){
+        number = document.querySelector("#number");
+    }
+    number.innerHTML = counter;
+}
+
+function interval(){
+        counter++;
+        // write some code here
+        if (counter === 60){
+          counter = 0
+        }
+
+        showNumber(counter);
+}
+
+setInterval(interval, 500);
